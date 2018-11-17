0bcae7aaff3a932a681d5ee390895aee6613b955
ondrej_kajinek <ondrej.kajinek@gmail.com>
2018-11-17 07:55:14 +0100

    refactor of supported media types

--- mpd_gainer.py
+++ mpd_gainer.py
@@ -6,6 +6,9 @@ from gainer import Gainer
 from lock import Lock, LockError
 from utils import error, info
 
+# SUPPORTED_MEDIA_TYPES = ("flac", "vorbis", "mp3")
+SUPPORTED_MEDIA_TYPES = ("flac", "mp3")
+
 
 def _check_options(options):
     if all((options.add_replay_gain, options.remove_replay_gain)):
@@ -18,8 +21,7 @@ def _check_options(options):
         options.add_replay_gain = True
 
     if options.all:
-        # for opt in ("flac", "vorbis", "mp3"):
-        for opt in ("flac", "mp3"):
+        for opt in SUPPORTED_MEDIA_TYPES:
             setattr(options, opt, True)
 
     if not any(
@@ -51,7 +53,7 @@ def _get_options():
     add_switch(parser, "--force", "-F")
     media_types = ("flac", "vorbis", "mp3")
     add_switch(parser, "--all")
-    for media_type in media_types:
+    for media_type in SUPPORTED_MEDIA_TYPES:
         add_switch(parser, "--%s" % media_type)
 
     options = parser.parse_args()
